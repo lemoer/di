991fdf83d6ac2ec4a5a76320e9bafebc0ed2d8a2
lemoer <leo.moerlein@gmail.com>
2015-02-12 04:27:42 +0100

    Changed the name of 'modules' to 'features', added instance injection, and flattened the di.di module to di

--- di-test.py
+++ di-test.py
@@ -5,7 +5,7 @@ import di
 class testDI(unittest.TestCase):
 
 	def setUp(self):
-		self.di = di.DI()
+		self.di = di
 
 	def testServiceSingletone(self):
 		# di.Service should be singletone!
@@ -28,41 +28,20 @@ class testDI(unittest.TestCase):
 	def testWildcard(self):
 		# Wildcards (*) at the end should return a list.
 
-		self.di.implements(di.Value, "Config/Val1")
-		self.di.implements(di.Value, "Config/Val2")
+		self.di.implements(123, "Config/Val1")
+		self.di.implements(234, "Config/Val2")
 
-		self.di.inject("Config/Val1").set(123)
-		self.di.inject("Config/Val2").set(234)
+		#self.di.inject("Config/Val1").set(123)
+		#self.di.inject("Config/Val2").set(234)
 
 		dictionary = self.di.inject("Config/*")
 
-		self.assertEqual(dictionary["Val1"].get(), 123)
-		self.assertEqual(dictionary["Val2"].get(), 234)
+		self.assertEqual(dictionary["Val1"], 123)
+		self.assertEqual(dictionary["Val2"], 234)
 
 		emptyList = self.di.inject("Another/*")
 		self.assertIs(type(emptyList), dict)
 		self.assertEqual(len(emptyList), 0)
-
-	def testList(self):
-		# The type of an empty di.List should be list not None
-		l = di.List()
-		
-		self.assertIsNot(l.get(), None)
-		self.assertIs(type(l.get()), list)
-
-		l.add('TestStr')
-
-		self.assertEqual(l.get()[0], 'TestStr')
-
-	def testDict(self):
-		# The type of an empty di.Dict should be dict not None
-		l = di.Dict()
-
-		self.assertIs(type(l.get()), dict)
-
-		l.put('test', 'val')
-
-		self.assertEqual(l.get()['test'], 'val')
 
 if __name__ == '__main__':
 	unittest.main()
--- di.py
+++ di.py
@@ -2,47 +2,53 @@
 
 import re
 
-class DI(object):
-	"""This class should provide the main functions for the DI-Pattern"""
-	def __init__(self):
-		super(DI, self).__init__()
-		self.modules = {}
-		self.staticInstances = {}
+features = {}
+staticInstances = {}
 
-	def implements(self, classType, moduleName):
-		self.modules[moduleName] = classType
+def implements(feature, featureName):
+	global features
+	global staticInstances
 
-	def inject(self, moduleName):
-		# Wildcards
-		if moduleName[-1] == "*":
-			reStr = '^' + moduleName[:-1] + '(.*)$'
-			reCompiled = re.compile(reStr)
+	features[featureName] = feature
 
-			result = {}
-			for key, val in self.modules.items():
-				reMatch = reCompiled.match(key)
-				
-				if reMatch is None:
-					continue
-				
-				key = reMatch.group(1)
-				longKey = moduleName[:-1] + key
-				result[key] = self.inject(longKey)
+	if type(feature) is not type:
+		staticInstances[featureName] = feature
 
-			return result
 
-		# Check for an existing service instance
-		if moduleName in self.staticInstances.keys():
-			return self.staticInstances[moduleName]
+def inject(featureName):
+	global features
+	global staticInstances
 
-		classType = self.modules[moduleName]
-		instance = classType()
+	# Wildcards
+	if featureName[-1] == "*":
+		reStr = '^' + featureName[:-1] + '(.*)$'
+		reCompiled = re.compile(reStr)
 
-		# If the class is a service save the instance
-		if classType.isStatic():
-			self.staticInstances[moduleName] = instance
+		result = {}
+		for key, val in features.items():
+			reMatch = reCompiled.match(key)
+			
+			if reMatch is None:
+				continue
+			
+			key = reMatch.group(1)
+			longKey =featureName[:-1] + key
+			result[key] = inject(longKey)
 
-		return instance
+		return result
+
+	# Check for an existing service instance
+	if featureName in staticInstances.keys():
+		return staticInstances[featureName]
+
+	classType = features[featureName]
+	instance = classType()
+
+	# If the class is a service save the instance
+	if classType.isStatic():
+		staticInstances[featureName] = instance
+
+	return instance
 
 # Singletone
 class Service(object):
@@ -55,37 +61,3 @@ class Controller(object):
 	@staticmethod
 	def isStatic():
 		return False
-
-class Value(object):
-	@staticmethod
-	def isStatic():
-		return True
-
-	def __init__(self):
-		self.value = None
-	
-	def get(self):
-		return self.value
-
-	def set(self, value):
-		self.value = value
-
-class List(Value):
-
-	def __init__(self):
-		self.value = []
-
-	def add(self, value):
-		if type(self.value) is list:
-			self.value += [value]
-
-class Dict(Value):
-
-	def __init__(self):
-		self.value = {}
-
-	def put(self, key, value):
-		if type(self.value) is dict:
-			self.value[key] = value
-
-di = DI()
